2f95d5b90fdf28ed59b71164c108d4c8b86c3cbb
Nguyễn Anh Tùng <atvn15@gmail.com>
2025-08-14 01:01:08 +0700

    add logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import "./../global.css"; // Ensure global styles are imported
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
